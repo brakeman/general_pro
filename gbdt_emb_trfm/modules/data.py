9396c2b8cb96e2d1e2f56dfe422fd59b2bc1f91a
qb <qibo_2018@outlook.com>
2019-09-12 10:12:04 +0800

    add leaf seq in data.py

--- gbdt_emb_trfm/modules/data.py
+++ gbdt_emb_trfm/modules/data.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 
 class CbData(Dataset):
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, num_trees, leaf_num_per_tree, gbdt_model, transform=None):
         """
         Args:
             root_dir (string): Directory with all the data.
@@ -19,8 +19,17 @@ class CbData(Dataset):
         self.y.index = range(len(self.y))
         self.keep_rows = self.y[~self.y.default_20.isnull()].index.tolist()
         self.x = self.x[self.keep_rows, :-1]
+
+        x = gbdt_model.predict(self.x, pred_leaf=True)  # [bs, 4000 trees]
+        to_add = np.arange(0, leaf_num_per_tree * num_trees, step=leaf_num_per_tree)
+        x = x + to_add  # 【bs, 4000】叶子序号
+        to_concat = np.ones(len(x))[:, np.newaxis] * leaf_num_per_tree * num_trees  # [CLS]编码
+        self.x = np.concatenate((to_concat, x), axis=1).astype(int)
+
         self.y = self.y.values[self.keep_rows, -1]
-        self.transform = transform
+        self.transform = transform  # gbdt_model
+        self.leaf_num_per_tree = leaf_num_per_tree
+        self.num_trees = num_trees
 
     def __len__(self):
         return len(self.x)
@@ -28,16 +37,17 @@ class CbData(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
+
         x = self.x[idx]
         y = self.y[idx]
-        sample = {'x': x, 'y': y}
         if self.transform:
-            sample = self.transform(sample)
+            x = self.transform(x)
+        sample = {'x': x, 'y': y}
         return sample
 
 
 if __name__ == '__main__':
-    cb = CbData(root_dir='...')
+    cb = CbData(root_dir='...', )
     loader = DataLoader(dataset=cb, batch_size=10000)
     for batch_ndx, sampl in enumerate(loader):
         print(sampl['y'])
